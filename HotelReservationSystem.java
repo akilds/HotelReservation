287380efb550d1076a41f6d4f6f60690dddb41ae
akilds <akilrainadesigan@gmail.com>
2021-04-15 09:32:37 +0530

    CREATED : Master Branch

---
+++ HotelReservationSystem.java
@@ -0,0 +1,7 @@
+public class HotelReservationSystem
+{
+ public static void main(String[] args)
+ {
+  System.out.println("Welcome to Hotel Reservation Program in HotelReservation class on Master Branch");
+ }
+}
